1c395e57e9f811b502b9cd09ebf05a53d5db7549
Asad Khan <asad_khan47@hotmail.co.uk>
2018-11-12 15:12:35 +0000

    Added cat page

--- lib/app.rb
+++ lib/app.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+set :session_secret, 'super secret'
 
 get '/' do
   "Hello dude!"
@@ -6,4 +7,10 @@ end
 
 get '/secret' do
   "It's Pickle RICK!"
+end
+
+get "/cat" do
+  "<div style='border: 3px dashed red'>
+    <img src='https://www.rd.com/wp-content/uploads/2016/04/01-cat-wants-to-tell-you-laptop.jpg'>
+   </div>"
 end
